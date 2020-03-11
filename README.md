9f884b486ae3f080b4ee1a86d1c34fd72c07a02f
lihini223 <lihininisansala95@gmail.com>
2020-03-12 01:04:44 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# friendlymarket-codelab-iqu1f5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/friendlymarket-codelab-iqu1f5)
---
+++ src/firebase-config.js
@@ -0,0 +1,15 @@
+/* 
+  Your Firebase config from the Firebase console 
+  https://firebase.google.com/docs/web/setup#config-object
+*/
+const firebaseConfig = {
+  apiKey: "AIzaSyDf2WPH8H45Qznga5W6CFF3g0y6aK5Y14E",
+  authDomain: "friendlymarket-18ec6.firebaseapp.com",
+  databaseURL: "https://friendlymarket-18ec6.firebaseio.com",
+  projectId: "friendlymarket-18ec6",
+  storageBucket: "friendlymarket-18ec6.appspot.com",
+  messagingSenderId: "549047673990",
+  appId: "1:549047673990:web:962526ed61e1d7ddfe1d07"
+};
+
+export default firebaseConfig;
